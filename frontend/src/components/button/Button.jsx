c5db54846daab6f11bf1431573bd424f9b801a0d
Andreas Warncke <112560055+andreas8151@users.noreply.github.com>
2023-03-28 17:03:43 +0200

    Delete frontend directory

--- frontend/src/components/button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import "./button.css";
-
-export default function Button({ title, type, onClick }) {
-  return (
-    <button className="button" type={type} onClick={onClick}>
-      {title}
-    </button>
-  );
-}
--- frontend/src/components/input/Input.jsx
+++
@@ -1,7 +0,0 @@
-import "./input.css";
-
-export default function Input({ type, onChange, value }) {
-  return (
-    <input className="input" value={value} typ={type} onChange={onChange} />
-  );
-}
