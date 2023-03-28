a6f9da51084cad499546a7d280c3233ed10b5865
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-03-28 16:24:17 +0200

    Revert "removed getfriendstodos"
    This reverts commit 62acf8a6ad8c0bd818ef437b3bd6c552848afefd.

--- backend/src/routes/friendRoute/friendsRoute.js
+++ backend/src/routes/friendRoute/friendsRoute.js
@@ -5,8 +5,12 @@ const { addFriend } = require("../../controllers/friendControllers/addFriend");
 const {
   getFriends,
 } = require("../../controllers/friendControllers/getFriends");
+const {
+  getFriendTodos,
+} = require("../../controllers/friendControllers/getFriendTodos");
 
 friendRoutes.get("/getFriends", getFriends);
 friendRoutes.post("/addFriend", addFriend);
+friendRoutes.get("/getFriendTodos", getFriendTodos);
 
 module.exports = { friendRoutes };
